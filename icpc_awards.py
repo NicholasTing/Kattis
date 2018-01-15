86116518529937c842b9bd47bc38817a4ff69354
Nicholas <nicholasting92@gmail.com>
2018-01-15 17:01:44 +0800

    Completed a problem

---
+++ icpc_awards.py
@@ -0,0 +1,18 @@
+#Coded by Jing Kun Ting
+#Coded on 15/1/2018
+#open.kattis.com problem : ICPC Awards
+#Python 3 Solution
+
+total_teams = int(input())
+universities = []
+
+while len(universities) < 12:
+
+    data = input().split()
+
+    uni = data[0]
+    team = data[1]
+
+    if uni not in universities:
+        universities.append(uni)
+        print(uni + " " + team)
