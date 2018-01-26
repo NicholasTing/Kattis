62fcab183c71e0c61a89fe641ccc0309699fd7c9
Nicholas <nicholasting92@gmail.com>
2018-01-26 14:31:54 +0800

    Solved

---
+++ Aaah/Aaah.py
@@ -0,0 +1,13 @@
+#Coded by Jing Kun Ting
+#Coded on 26/1/2018
+#open.kattis.com problem : Aaah!
+#Problem: https://open.kattis.com/problems/aaah
+#Python 3 Solution
+
+jon = input()
+doctor = input()
+
+if len(jon) >= len(doctor):
+    print("go")
+else:
+    print("no")
--- Zamka/zamka.py
+++ Zamka/zamka.py
@@ -1,3 +1,7 @@
 L = raw_input()
 D = raw_input()
 Z = raw_input()
+
+lowest = int(L)
+highest = int(D)
+curr_num = int(Z)
