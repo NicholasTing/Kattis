823412cd79d0b3244cb1f70b159ee1c71c552921
Nicholas <nicholasting92@gmail.com>
2018-01-27 23:08:19 +0800

    Solved oddities

---
+++ Oddities/oddities.cpp
@@ -0,0 +1,28 @@
+#Coded by Jing Kun Ting
+#Coded on ?/?/17
+#open.kattis.com problem : Oddities
+#Problem : https://open.kattis.com/problems/oddities
+#Coded in CPP
+
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+int N;
+
+int
+main(){
+    scanf("%d",&N);
+    int num = 0;
+    while(N--){
+        scanf("%d",&num);
+        if(num % 2 == 0){
+            printf("%d is even\n",num);
+        }
+        else{
+            printf("%d is odd\n",num);
+        }
+    }
+    return 0;
+}
