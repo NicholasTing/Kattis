e71f5fbe1a54e583faad91911cc95f41a805140c
nicholasting92 <nicholasting92@gmail.com>
2018-12-22 21:55:30 +1100

    Incomplete

---
+++ t9_spelling.py
@@ -0,0 +1,25 @@
+total = int(input())
+
+two = ["abc"]
+three = ["def"]
+four = ["ghi"]
+five = ["jkl"]
+six = ["mno"]
+seven = ["pqrs"]
+eight = ["tuv"]
+nine = ["wxyz"]
+
+while total != 0:
+    words = input()
+    try:
+        words = words.split(" ")
+        final_string = ""
+        
+    except:
+        word = words
+        for letter in word:
+            pass
+
+    print(final_string)
+
+    total -= 1
